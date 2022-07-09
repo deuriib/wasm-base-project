e03136c2c1ac6e29755b73119adce458438bb2ad
Deuri Vasquez <deuriib@gmail.com>
2022-07-09 13:08:53 -0400

    update Program.cs

--- WasmBaseProjectApp/Program.cs
+++ WasmBaseProjectApp/Program.cs
@@ -20,7 +20,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddHttpClient<EmployeeService>(config =>
 {
-    config.BaseAddress = new Uri(Environment.GetEnvironmentVariable("API_URL")!);
+    config.BaseAddress = new Uri("https://localhost:5001");
 });
 
 builder.Services.AddMudServices(config =>
